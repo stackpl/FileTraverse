cd5d6e237f6bb3074fcc9b09836133c196a601c7
DariuszChilimoniuk <DariuszChilimoniuk@users.noreply.github.com>
2022-01-23 21:59:09 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    FileTraverse
+    ~~~~~
+    YAML and XML reader based on callbacks
+    :copyright: (c) 2022 by Darek Chilimoniuk, https://stack.pl
+    :license: MIT, see LICENSE for more details.
+"""
+from FileTraverse import Node, BaseYamlListener
 
+__version__ = '0.0.1'
